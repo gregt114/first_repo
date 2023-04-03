68c8e25fbe2d96e775443802faaa122717edae5a
greg <gregturnberg@gmail.com>
2023-04-03 19:22:43 -0400

    Standard recursive implementation of Fibonacci numbers

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("testing 123")
+
+def fibonacci(n: int) -> int:
+    """
+    Recursive implementation to calculate the n-th Fibonacci number
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+for i in range(32):
+    print(f"{i} : {fibonacci(i)}")
