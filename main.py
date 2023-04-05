95a0f54d8abc496288346b7a7481337f7308735c
greg <gregturnberg@gmail.com>
2023-04-04 22:48:08 -0400

    fibonacci formula implementation

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("testing 123")
+
+import math
+
+phi = (1 + math.sqrt(5)) / 2
+
+
+def fibonacci(n: int) -> int:
+    """
+    Compute n-th Fibonacci number using formula
+    """
+    result = (phi**n - (-phi)**(-n)) / math.sqrt(5)
+    return round(result)
+
+for i in range(0, 32):
+    print(f"{i} : {fibonacci(i)}")
